fc39ac0d7ce1ef1fb712b348a2756fce4b0890b5
criptom1234 <craken.1994@gmail.com>
2025-07-01 11:11:09 -0500

    primer cambio

---
+++ Config/Config.php
@@ -0,0 +1,119 @@
+<?php
+// const BASE_URL = "https://wakamieventos.com";
+//const BASE_URL = 'http://localhost/tienda-online-saas/';
+const BASE_URL = 'http://3.85.114.254/tienda-online-saas/';
+
+//Zona horaria
+date_default_timezone_set('America/Lima');
+
+//Datos de conexión a Base de Datos
+// const DB_HOST = "localhost";
+// const DB_NAME = "wakamiev_db2";
+// const DB_USER = "wakamiev_user";
+// const DB_PASSWORD = "KpY17p5z3oxx";
+// const DB_CHARSET = "utf8";
+
+// const DB_HOST = "localhost";
+// const DB_NAME = "wakami";
+// const DB_USER = "root";
+// const DB_PASSWORD = "";
+// const DB_CHARSET = "utf8";
+
+
+const DB_HOST = "mariadb-ingenieria-software-2.cklqmyee440q.us-east-1.rds.amazonaws.com";
+const DB_NAME = "db_tienda_is2";
+const DB_USER = "admin";
+const DB_PASSWORD = "12345678";
+const DB_CHARSET = "utf8";
+//Para envío de correo
+const ENVIRONMENT = 1; // Local: 0, Produccón: 1;
+
+//Deliminadores decimal y millar Ej. 24,1989.00
+const SPD = ".";
+const SPM = ",";
+
+//Simbolo de moneda
+const SMONEY = "S/";
+const CURRENCY = "PEN";
+
+//Api PayPal
+//SANDBOX PAYPAL
+const URLPAYPAL = "https://api-m.sandbox.paypal.com";
+const IDCLIENTE = "";
+const SECRET = "";
+//LIVE PAYPAL
+//const URLPAYPAL = "https://api-m.paypal.com";
+//const IDCLIENTE = "";
+//const SECRET = "";
+
+//Datos envio de correo
+const NOMBRE_REMITENTE = "Tienda Virtual";
+const EMAIL_REMITENTE = "no-reply@tecomsis.com";
+const NOMBRE_EMPESA = "Tienda Virtual Wakami Eventos";
+const WEB_EMPRESA = " ";
+
+const DESCRIPCION = "Wakami eventos, empresa dedicada a eventos y venta de artículos.";
+const SHAREDHASH = "TiendaVirtual";
+
+//Datos Empresa
+const DIRECCION = "Imperial, Cañete";
+const TELEMPRESA = "993403860";
+const WHATSAPP = "998889563";
+const EMAIL_EMPRESA = "";
+const EMAIL_PEDIDOS = "wakami@gmail.com";
+const EMAIL_SUSCRIPCION = "";
+const EMAIL_CONTACTO = "";
+
+const CAT_SLIDER = "1,2,3,4,5";
+const CAT_BANNER = "6,7,8";
+const CAT_FOOTER = "1,2,3,4,5";
+
+//Datos para Encriptar / Desencriptar
+const KEY = 'Eventos';
+const METHODENCRIPT = "AES-128-ECB";
+
+//Envío
+const COSTOENVIO = 5;
+
+//Módulos
+const MDASHBOARD = 1;
+const MUSUARIOS = 2;
+const MCLIENTES = 3;
+const MPRODUCTOS = 4;
+const MPEDIDOS = 5;
+const MCATEGORIAS = 6;
+const MSUSCRIPTORES = 7;
+const MDCONTACTOS = 8;
+const MDPAGINAS = 9;
+
+//Páginas
+const PINICIO = 1;
+const PTIENDA = 2;
+const PCARRITO = 3;
+const PNOSOTROS = 4;
+const PCONTACTO = 5;
+const PPREGUNTAS = 6;
+const PTERMINOS = 7;
+const PSUCURSALES = 8;
+const PERROR = 9;
+const PBLOG = 10;
+
+//Roles
+const RADMINISTRADOR = 1;
+const RSUPERVISOR = 2;
+const RCLIENTES = 3;
+
+const STATUS = array('Completo', 'Aprobado', 'Cancelado', 'Reembolsado', 'Pendiente', 'Entregado');
+
+//Productos por página
+const CANTPORDHOME = 8;
+const PROPORPAGINA = 4;
+const PROCATEGORIA = 4;
+const PROBUSCAR = 4;
+
+//REDES SOCIALES
+const FACEBOOK = "https://www.facebook.com/eventoswakami";
+const INSTAGRAM = "#";
+
+//HORARIO
+const HORARIO = "Abierto: 8:00 a.m | Cerrado: 8:00 p.m";
