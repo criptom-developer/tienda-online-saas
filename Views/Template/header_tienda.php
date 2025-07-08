edfbbc259de71e55ac8730bd6ca01865a2da6aa7
criptom1234 <craken.1994@gmail.com>
2025-07-08 15:29:14 -0500

    cambio 16

--- Views/Template/header_tienda.php
+++ Views/Template/header_tienda.php
@@ -186,7 +186,7 @@
 	                    <span class="icon-bar"></span>
 	                    <span class="icon-bar"></span>
 	                </button>
-	                <a class="navbar-brand page-scroll" href="index.php">Wakami2</a>
+	                <!-- <a class="navbar-brand page-scroll" href="index.php">Wakami2</a> -->
 	            </div>
 
 	            <!-- Collect the nav links, forms, and other content for toggling -->
