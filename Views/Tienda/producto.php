e18f47d2076e146bf045d52eaf82e834b879a2a7
criptom1234 <craken.1994@gmail.com>
2025-07-22 14:52:27 -0500

    c79

--- Views/Tienda/producto.php
+++ Views/Tienda/producto.php
@@ -90,7 +90,7 @@ $urlShared = base_url()."/tienda/producto/".$arrProducto['idproducto']."/".$arrP
 							<?= $arrProducto['nombre']; ?>
 						</h4>
 						<span class="mtext-106 cl2">
-							<?= SMONEY.formatMoney($arrProducto['precio']) . ' (precio por un año)'?>
+							<?= SMONEY.formatMoney($arrProducto['precio']) ?>
 						</span>
 						<!-- <p class="stext-102 cl3 p-t-23"></p> -->
 						<?= $arrProducto['descripcion']; ?>
